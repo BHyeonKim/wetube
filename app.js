607fa7044efc00898c420a427bf4e01ce1a62f7a
BoHyunK <rlaqhguse@gmail.com>
2020-02-22 20:39:34 +0900

    install pug

--- app.js
+++ app.js
@@ -10,10 +10,11 @@ import videoRouter from "./routers/videoRouter"
 import routes from "./routes"
 const app = express();
 
+app.set("view engine", "pug");
 app.use(helmet());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
 app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
 app.use(morgan("dev"));
 
 app.use("/", globalRouter);
