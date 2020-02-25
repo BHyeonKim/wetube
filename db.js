3a3f9f15154d7bba49ec502333ca124b8dd7f924
BoHyunK <rlaqhguse@gmail.com>
2020-02-26 02:11:25 +0900

    Home Controller

---
+++ db.js
@@ -0,0 +1,54 @@
+export const videos = [
+    {
+        id:324393,
+        title: "Video awesome",
+        description:"This is something I love",
+        views:24,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id:2341234,
+            name:"Hyeon",
+            email:"rlaqhguse@gmail.com"
+        
+        }
+    },
+    {
+        id:1234213,
+        title: "Video super",
+        description:"This is something I love",
+        views:24,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id:2341234,
+            name:"Hyeon",
+            email:"rlaqhguse@gmail.com"
+        
+        }
+    },
+    {
+        id:635432,
+        title: "Video perfect",
+        description:"This is something I love",
+        views:24,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id:2341234,
+            name:"Hyeon",
+            email:"rlaqhguse@gmail.com"
+        
+        }
+    },
+    {
+        id:1543156,
+        title: "Video nice",
+        description:"This is something I love",
+        views:24,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id:2341234,
+            name:"Hyeon",
+            email:"rlaqhguse@gmail.com"
+        
+        }
+    }
+]
