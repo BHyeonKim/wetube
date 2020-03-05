bec145825a0c62c148bd976123ad8499659ba734
BoHyunK <rlaqhguse@gmail.com>
2020-03-05 20:47:52 +0900

    Home Controller Finished

---
+++ models/Video.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const VideoSchema = new mongoose.Schema({
+    fileUrl: {
+        type: String,
+        required: "File URL is required"
+    },
+    title: {
+        type:String,
+        required: "Title is requried"
+    },
+    description: String,
+    views: {
+        type: Number,
+        defualt: 0
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    comments: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref:"Comment"
+    }]
+})
+
+const model = mongoose.model("Video", VideoSchema);
+export default model;
